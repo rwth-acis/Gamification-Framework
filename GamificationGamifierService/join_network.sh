31b7e189d471d06b7e601e7cf28148db4d190c01
Felix Jordan <felix.jordan@buddy-app.de>
2022-04-05 14:09:56 +0200

    fix: update generated scripts to be consistent with gamification.config after running main.sh -s

--- GamificationGamifierService/join_network.sh
+++ GamificationGamifierService/join_network.sh
@@ -1,3 +1,3 @@
 
-java -cp "lib/*:service/*" i5.las2peer.tools.L2pNodeLauncher -p 9115 -b 137.226.232.171:9010 --observer uploadStartupDirectory\(\'etc/startup\'\) \
+java -cp "lib/*:service/*" i5.las2peer.tools.L2pNodeLauncher -p 9114 -b 137.226.232.171:9010 uploadStartupDirectory\(\'etc/startup\'\) \
 startService\(\'i5.las2peer.services.gamificationGameService.GamificationGameService@0.1\',\'gamificationgamepass\'\) startWebConnector interactive
